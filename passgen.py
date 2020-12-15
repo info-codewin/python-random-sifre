d627e2e83a83ac64aeb45941301cb275874c869a
info-codewin <75925152+info-codewin@users.noreply.github.com>
2020-12-15 12:50:58 +0300

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,3 +1,5 @@
+#https://discord.gg/cMpeHRYCHN
+#Made by WİN CODE
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
